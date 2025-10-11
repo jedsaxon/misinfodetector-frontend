31651d64094476924c181b07bc407164d8e735f8
Jed Saxon <jed-sax@proton.me>
2025-10-11 11:39:18 +1100

    view all posts in correct format

--- src/pages/PostsPage.tsx
+++ src/pages/PostsPage.tsx
@@ -1,10 +1,20 @@
 import { Alert, AlertDescription, AlertTitle } from "@/components/ui/alert";
+import { Button } from "@/components/ui/button";
+import { ButtonGroup } from "@/components/ui/button-group";
+import {
+  Card,
+  CardContent,
+  CardFooter,
+  CardHeader,
+  CardTitle,
+} from "@/components/ui/card";
 import Header from "@/components/ui/header";
 import { SidebarTrigger } from "@/components/ui/sidebar";
+import { Skeleton } from "@/components/ui/skeleton";
 import VerticalSeparator from "@/components/ui/verticalseparator";
 import { DetailedApiError } from "@/services/api-utils";
 import { fetchPosts, Post } from "@/services/comment-service";
-import { AlertCircleIcon, MessagesSquare } from "lucide-react";
+import { AlertCircleIcon, HeartIcon, MessagesSquare } from "lucide-react";
 import { useEffect, useState } from "react";
 
 export default function PostsPage() {
@@ -12,8 +22,6 @@ export default function PostsPage() {
   const [apiError, setApiError] = useState<DetailedApiError | undefined>(
     undefined,
   );
-
-  console.log(posts);
 
   useEffect(() => {
     const handlePostFetch = async () => {
@@ -32,6 +40,12 @@ export default function PostsPage() {
     handlePostFetch();
   }, []);
 
+  const postComponents = posts ? (
+    posts.map((p) => <Post post={p} key={p.id} />)
+  ) : (
+    <PostSkeleton />
+  );
+
   return (
     <>
       <Header className="mb-3">
@@ -40,8 +54,9 @@ export default function PostsPage() {
         <MessagesSquare className="mx-1" color="#3584e4" />
         Posts
       </Header>
-      <div className="flex flex-col gap-2 m-auto" style={{ maxWidth: "512px" }}>
+      <div className="flex flex-col gap-5 m-auto" style={{ maxWidth: "512px" }}>
         {apiError && <ApiError error={apiError} />}
+        {postComponents}
       </div>
     </>
   );
@@ -54,5 +69,33 @@ function ApiError({ error }: { error: DetailedApiError }) {
       <AlertTitle>{error.title}</AlertTitle>
       {error && <AlertDescription>{error.description}</AlertDescription>}
     </Alert>
+  );
+}
+
+function PostSkeleton() {
+  return (
+    <div className="flex flex-col gap-2">
+      <Skeleton style={{ width: "512px", height: "calc(1.5rem * 1)" }} />
+      <Skeleton style={{ width: "512px", height: "calc(1.5rem * 4)" }} />
+    </div>
+  );
+}
+
+function Post({ post }: { post: Post }) {
+  return (
+    <Card>
+      <CardHeader>
+        <CardTitle>{post.username}</CardTitle>
+      </CardHeader>
+      <CardContent>
+        <p>{post.message}</p>
+      </CardContent>
+      <CardFooter>
+        <ButtonGroup>
+          <Button variant="outline"><HeartIcon /></Button>
+          <Button variant="outline">Details</Button>
+        </ButtonGroup>
+      </CardFooter>
+    </Card>
   );
 }
